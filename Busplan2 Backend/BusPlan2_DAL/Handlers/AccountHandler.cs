eca0a78894215063c17b1e550d79af21302b2db8
Noah Koole <Noahkoole@gmail.com>
2021-03-25 15:02:02 +0100

    Changed login returning dto to returning jwt token

--- Busplan2 Backend/BusPlan2_DAL/Handlers/AccountHandler.cs
+++ Busplan2 Backend/BusPlan2_DAL/Handlers/AccountHandler.cs
@@ -1,7 +1,12 @@
-﻿using BusPlan2_DAL.DTOs;
+﻿using Backend_DAL;
+using BusPlan2_DAL.DTOs;
+using Microsoft.IdentityModel.Tokens;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace BusPlan2_DAL.Handlers
@@ -28,29 +33,28 @@ namespace BusPlan2_DAL.Handlers
 
         }
 
-        public AccountDTO Login(int loginCode, string password)
+        public string Login(int loginCode, string password)
         {
+            string AccountID;
+            int LoginCode;
             using var connection = Connection.GetConnection();
-
             try
             {
                 using (connection)
                 {
                     connection.Open();
-                    MySqlCommand command = new MySqlCommand("SELECT * FROM Account WHERE LoginCode = @LoginCode AND Password = @Password", connection);
+                    MySqlCommand command = new MySqlCommand("SELECT AccountID, LoginCode FROM Account WHERE LoginCode = @LoginCode AND Password = @Password", connection);
                     command.Parameters.AddWithValue("@LoginCode", loginCode);
                     command.Parameters.AddWithValue("@Password", password);
-                    var reader = command.ExecuteReader();
+                    DataSet ds = new DataSet();
+                    MySqlDataAdapter da = new MySqlDataAdapter(command);
+                    da.Fill(ds);
+                    connection.Close();
 
-                    if(!reader.Read()) throw new Exception("Incorrect username or password.");
-                    var accountobj = new AccountDTO()
-                    {
-                        AccountID = reader.GetInt32("AccountID"),
-                        LoginCode = reader.GetInt32("LoginCode"),
-                        Name = reader.GetString("Name"),
-                        Team = reader.GetInt32("Team")
-                    };
-                    return accountobj;
+                    if (ds.Tables[0].Rows.Count < 1) return null;
+                    DataRow row = ds.Tables[0].Rows[0];
+                    AccountID = Convert.ToString(row["AccountID"]);
+                    LoginCode = Convert.ToInt32(row["LoginCode"]);
                 }
             }
             catch (MySqlException)
@@ -58,6 +62,27 @@ namespace BusPlan2_DAL.Handlers
                 connection.Close();
                 throw new Exception("An unexpected error occured.");
             }
+
+            AppSettings _appSettings = new AppSettings() { Key = "kprlcvattbtitafgjawqyjjwjsxhzqvxsrzfnlpvxpdcdzwfsh" }; // A random string to encrypt the token
+
+            // Generate JWT with these credentials
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                        new Claim(ClaimTypes.Name, AccountID),
+                        new Claim(ClaimTypes.Email,Convert.ToString(LoginCode)),
+                }),
+                Expires = DateTime.UtcNow.AddDays(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var result = tokenHandler.CreateToken(tokenDescriptor);
+            string token = tokenHandler.WriteToken(result);
+
+            // Return the found account in the form of a jwt token
+            return token;
         }
     }
 }
