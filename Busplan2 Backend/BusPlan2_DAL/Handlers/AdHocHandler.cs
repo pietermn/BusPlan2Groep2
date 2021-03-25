7cb96bf53a77a0392e61a1eb6c4d1120ae1f3c97
GivanWiggers <436715@student.fontys.nl>
2021-03-25 13:39:41 +0100

    AdhocHandler +  DTO CleaningHanler + DTO Connection undone, cause already merged

--- Busplan2 Backend/BusPlan2_DAL/Handlers/AdHocHandler.cs
+++ Busplan2 Backend/BusPlan2_DAL/Handlers/AdHocHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusPlan2_DAL.DTOs;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,24 +8,100 @@ namespace BusPlan2_DAL.Handlers
 {
     public class AdHocHandler
     {
-        public void Create()
+        public List<AdHocDTO> GetAllAdHoc()
         {
+            List<AdHocDTO> adHocDTOList = new List<AdHocDTO>();
+            using var connection = Connection.GetConnection();
+            {
+                connection.OpenAsync();
 
+                using var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM `AdHoc`";
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        adHocDTOList.Add(
+                                new AdHocDTO(
+                                    reader.GetInt32("AdHocID"),
+                                    reader.GetInt32("BusID"),
+                                    reader.GetInt32("Type"),
+                                    reader.GetInt32("Team"),
+                                    reader.GetString("Description"),
+                                    reader.GetDateTime("TimeDOne")
+                                )
+                            );
+
+                    }
+                }
+                return adHocDTOList;
+            }
         }
 
-        public void Read()
+        public AdHocDTO GetAdHoc(int busid)
         {
+            using var connection = Connection.GetConnection();
+            {
+                connection.OpenAsync();
 
+                using var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM `AdHoc` WHERE BusID = @busID";
+                command.Parameters.AddWithValue("@busID", busid);
+
+                var reader = command.ExecuteReader();
+
+                if (!reader.Read()) return null;
+
+                var adhocobj = new AdHocDTO()
+                {
+                    AdHocID = reader.GetInt32("AdHocID"),
+                    BusID = reader.GetInt32("BusID"),
+                    Type = reader.GetInt32("Type"),
+                    Team = reader.GetInt32("Team"),
+                    Description = reader.GetString("Description"),
+                    TimeDone = reader.GetDateTime("TimeDOne")
+                };
+                return adhocobj;
+            }
         }
 
-        public void Update()
+        public bool UpdateCleaning(int busID, int type,string description, DateTime timeDone, int team)
         {
+            using var connection = Connection.GetConnection();
+            {
+                connection.OpenAsync();
 
+                using var command = connection.CreateCommand();
+                command.CommandText = "UPDATE `AdHoc` SET Type=@type, Team=@team, Description=@description TimeDone=@timeDone WHERE BusID=@busid";
+                command.Parameters.AddWithValue("@Type", type);
+                command.Parameters.AddWithValue("@Team", team);
+                command.Parameters.AddWithValue("@description", description);
+                command.Parameters.AddWithValue("@timeDone", timeDone);
+                command.Parameters.AddWithValue("@busid", busID);
+
+                command.ExecuteNonQueryAsync();
+                connection.CloseAsync();
+
+                return true;
+            }
         }
 
-        public void Delete()
+        public bool DeleteCleaning(int adhocID)
         {
+            using var connection = Connection.GetConnection();
+            {
+                connection.OpenAsync();
 
+                using var command = connection.CreateCommand();
+                command.CommandText = "DELETE * FROM `AdHoc` WHERE AdHocID = @adhocID";
+                command.Parameters.AddWithValue("@adhocID", adhocID);
+
+                command.ExecuteNonQueryAsync();
+                connection.CloseAsync();
+
+                return true;
+            }
         }
     }
 }
--- Busplan2 Backend/BusPlan2_DAL/Handlers/CleaningHandler.cs
+++ Busplan2 Backend/BusPlan2_DAL/Handlers/CleaningHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusPlan2_DAL.DTOs;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,24 +8,96 @@ namespace BusPlan2_DAL.Handlers
 {
     public class CleaningHandler
     {
-        public void Create()
+        public List<CleaningDTO> GetAllCleaning()
         {
+            List<CleaningDTO> cleaningDTOList = new List<CleaningDTO>();
+            using var connection = Connection.GetConnection();
+            {
+                connection.OpenAsync();
 
+                using var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM `Cleaning`";
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        cleaningDTOList.Add(
+                                new CleaningDTO(
+                                    reader.GetInt32("CleaningID"),
+                                    reader.GetInt32("BusID"),
+                                    reader.GetInt32("CleanedBy"),
+                                    reader.GetDateTime("TimeCleaned"),
+                                    reader.GetInt32("Status")
+                                )
+                            );
+                    }
+                }
+                return cleaningDTOList;
+            }
         }
 
-        public void Read()
+        public CleaningDTO GetCleaning(int busid)
         {
+            using var connection = Connection.GetConnection();
+            {
+                connection.OpenAsync();
 
+                using var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM `Cleaning` WHERE BusID = @busID";
+                command.Parameters.AddWithValue("@busID", busid);
+
+                var reader = command.ExecuteReader();
+
+                if (!reader.Read()) return null;
+
+                var cleaningobj = new CleaningDTO()
+                {
+                    CleaningID = reader.GetInt32("CleaningID"),
+                    BusID = reader.GetInt32("BusID"),
+                    CleanedBy = reader.GetInt32("CleanedBy"),
+                    TimeCleaned = reader.GetDateTime("TimeCleaned"),
+                    Status = reader.GetInt32("Status")
+                };
+                return cleaningobj;
+            }
         }
 
-        public void Update()
+        public bool UpdateCleaning(int busID, int cleanedBy, DateTime timeCleaned, int status)
         {
+            using var connection = Connection.GetConnection();
+            {
+                connection.OpenAsync();
 
+                using var command = connection.CreateCommand();
+                command.CommandText = "UPDATE `Cleaning` SET CleanedBy=@cleanedby, TimeCleaned=@timecleaned, Status=@status WHERE BusID=@busid";
+                command.Parameters.AddWithValue("@cleanedby", cleanedBy);
+                command.Parameters.AddWithValue("@timecleaned", timeCleaned);
+                command.Parameters.AddWithValue("@status", status);
+                command.Parameters.AddWithValue("@busid", busID);
+
+                command.ExecuteNonQueryAsync();
+                connection.CloseAsync();
+
+                return true;
+            }
         }
 
-        public void Delete()
+        public bool DeleteCleaning(int cleaningID)
         {
+            using var connection = Connection.GetConnection();
+            {
+                connection.OpenAsync();
 
+                using var command = connection.CreateCommand();
+                command.CommandText = "DELETE * FROM `Cleaning` WHERE CleaningID = @cleaningid";
+                command.Parameters.AddWithValue("@cleanedby", cleaningID);
+
+                command.ExecuteNonQueryAsync();
+                connection.CloseAsync();
+
+                return true;
+            }
         }
     }
 }
