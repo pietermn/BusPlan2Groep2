1a548cacd68e44202f69d02b71dc734be04ca86a
Mullen,Pieter P.A.J. van der <pieter.vd.mullen@gmail.com>
2021-03-25 12:50:18 +0100

    Added Methods to Bus related classes

--- Busplan2 Backend/BusPlan2_DAL/Handlers/BusHandler.cs
+++ Busplan2 Backend/BusPlan2_DAL/Handlers/BusHandler.cs
@@ -1,29 +1,154 @@
 ﻿using System;
+using System.Data;
+using MySql.Data;
+using BusPlan2_DAL.DTOs;
 using System.Collections.Generic;
-using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace BusPlan2_DAL.Handlers
 {
     public class BusHandler
     {
-        public void Create()
-        {
 
+        public bool Create(DateTime periodicCleaning, DateTime periodicMaintenance, int busNumber, int batteryLevel, int status)
+        {
+            using var connection = Connection.GetConnection();
+            {
+                try
+                {
+                    using var command = connection.CreateCommand();
+
+                    command.CommandText = "INSERT INTO Bus(BusNumber, BatteryLevel, Status, PeriodicCleaning, PeriodicMaintenance) VALUES(@busNumber, @batteryLevel, @status, @periodicCleaning, @periodicMaintenance);";
+                    command.Parameters.AddWithValue("@busNumber", busNumber);
+                    command.Parameters.AddWithValue("@batteryLevel", batteryLevel);
+                    command.Parameters.AddWithValue("@status", status);
+                    command.Parameters.AddWithValue("@periodicCleaning", periodicCleaning);
+                    command.Parameters.AddWithValue("@periodicMaintenance", periodicMaintenance);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    return true;
+                }
+                catch { connection.Close(); return false; }
+            }
         }
 
-        public void Read()
-        {
 
+        public BusDTO Read(int busID)
+        {
+            BusDTO bus = new BusDTO();
+            using var connection = Connection.GetConnection();
+            {
+                using var command = connection.CreateCommand();
+
+                command.CommandText = "SELECT * FROM Bus WHERE BusID = @busID";
+                command.Parameters.AddWithValue("@busID", busID);
+
+                connection.Open();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        bus = new BusDTO(
+                            reader.GetInt32("BusID"),
+                            reader.GetDateTime("PeriodicCleaning"),
+                            reader.GetDateTime("PeriodicMaintenance"),
+                            reader.GetInt32("BusNumber"),
+                            reader.GetInt32("BatteryLevel"),
+                            reader.GetInt32("Status")
+                            );
+                    }
+                }
+
+                connection.Close();
+                return bus;
+            }
         }
 
-        public void Update()
-        {
 
+        public List<BusDTO> ReadAll()
+        {
+            List<BusDTO> buses = new List<BusDTO>();
+            using var connection = Connection.GetConnection();
+            {
+                using var command = connection.CreateCommand();
+
+                command.CommandText = "SELECT * FROM Bus";
+
+                connection.Open();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        buses.Add(new BusDTO(
+                            reader.GetInt32("BusID"),
+                            reader.GetDateTime("PeriodicCleaning"),
+                            reader.GetDateTime("PeriodicMaintenance"),
+                            reader.GetInt32("BusNumber"),
+                            reader.GetInt32("BatteryLevel"),
+                            reader.GetInt32("Status")
+                            ));
+                    }
+                }
+
+                connection.Close();
+                return buses;
+            }
         }
 
-        public void Delete()
-        {
 
+        public bool Update(BusDTO bus)
+        {
+            using var connection = Connection.GetConnection();
+            {
+                try
+                {
+                    using var command = connection.CreateCommand();
+
+                    command.CommandText = "UPDATE Bus Set PeriodicCleaning = @periodicCleaning, PeriodicMaintenance = @periodicMaintenance, BusNumber = @busNumber, BatteryLevel = @batteryLevel, Status = @status WHERE BusID = @busID;";
+                    command.Parameters.AddWithValue("@busID", bus.BusID);
+                    command.Parameters.AddWithValue("@periodicCleaning", bus.PeriodicCleaning);
+                    command.Parameters.AddWithValue("@periodicMaintenance", bus.PeriodicMaintenance);
+                    command.Parameters.AddWithValue("@busNumber", bus.BusNumber);
+                    command.Parameters.AddWithValue("@batteryLevel", bus.BatteryLevel);
+                    command.Parameters.AddWithValue("@status", bus.Status);
+                    
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return true;
+                }
+                catch { connection.Close(); return false; }
+            }
         }
+
+
+        public bool Delete(int busID)
+        {
+            using var connection = Connection.GetConnection();
+            {
+                try
+                {
+                    using var command = connection.CreateCommand();
+
+                    command.CommandText = "DELETE FROM Bus WHERE BusID = @busID;";
+                    command.Parameters.AddWithValue("@busID", busID);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return true;
+                }
+                catch { connection.Close(); return false; }
+            }
+        }
+
+
     }
 }
---
+++ Busplan2 Backend/BusPlan2_DAL/Handlers/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace BusPlan2_DAL.Handlers
+{
+    public class Connection
+    {
+        public static MySqlConnection GetConnection()
+        {
+            MySqlConnection GeneralConnection = new MySqlConnection("Server=localhost;Uid=user;Database=busplanDB;Pwd=root;Port=3307;");
+            return GeneralConnection;
+        }
+    }
+}
