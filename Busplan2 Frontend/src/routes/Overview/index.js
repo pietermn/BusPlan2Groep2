a5895f87a77110c0423279c6fc7e2f840da9a956
Noah Koole <Noahkoole@gmail.com>
2021-04-08 13:35:28 +0200

    Started on bus layout

--- Busplan2 Frontend/src/routes/Overview/index.js
+++ Busplan2 Frontend/src/routes/Overview/index.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import BusSideBar from "../../components/BusSideBar";
 import { Context as BusContext } from "../../redux/context/BusContext";
-import Draggable, { DraggableCore } from "react-draggable";
 import "../../Style/busPagesStyles.css";
 
 const Overview = () => {
@@ -14,10 +13,11 @@ const Overview = () => {
   return (
     <div className="overview_full_page">
       {state.busses && <BusSideBar Busses={state.busses} />}
-      <Draggable position={{ x: 0, y: 0 }}>
-        <p style={{position: "absolute"}}>Drag</p>
-      </Draggable>
-      <div className="busmap"></div>
+      <div className="busmap">
+          <div className="grid">
+            <div id="busImage"></div>
+          </div>
+      </div>
     </div>
   );
 };
