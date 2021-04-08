07e65461da1b94a5c1492623ba6c291c373c47a0
Noah Koole <Noahkoole@gmail.com>
2021-04-08 15:40:45 +0200

    Added coordinates to json file to read out

--- Busplan2 Frontend/src/routes/Overview/index.js
+++ Busplan2 Frontend/src/routes/Overview/index.js
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect } from "react";
 import BusSideBar from "../../components/BusSideBar";
 import { Context as BusContext } from "../../redux/context/BusContext";
+import GreyBus from "../../Images/GreyBus.png";
 import "../../Style/busPagesStyles.css";
+const Coords = require("../../components/ParkingSpaceCoords.json");
 
 const Overview = () => {
   const { state, GetAllBusses } = useContext(BusContext);
-
   useEffect(() => {
     GetAllBusses();
   }, []);
@@ -14,9 +15,96 @@ const Overview = () => {
     <div className="overview_full_page">
       {state.busses && <BusSideBar Busses={state.busses} />}
       <div className="busmap">
-          <div className="grid">
-            <div id="busImage"></div>
+        <div className="grid">
+          <div id="busImage">
+            {Coords[0].parkingspaces.map((value, index) => {
+              return (
+                <img
+                  key={index}
+                  id="normal"
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
+            {Coords[1].parkingspaces.map((value, index) => {
+              return (
+                <img
+                  key={index}
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
+            {Coords[2].parkingspaces.map((value, index) => {
+              return (
+                <img
+                  id="slow"
+                  key={index}
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
+            {Coords[3].parkingspaces.map((value, index) => {
+              return (
+                <img
+                  id="sideways"
+                  key={index}
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
+            {Coords[4].parkingspaces.map((value, index) => {
+              return (
+                <img
+                  key={index}
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
+            {Coords[5].parkingspaces.map((value, index) => {
+              return (
+                <img
+                  key={index}
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
+            {Coords[6].parkingspaces.map((value, index) => {
+              return (
+                <img
+                  key={index}
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
+            {Coords[7].parkingspaces.map((value, index) => {
+              return (
+                <img
+                id="sideways"
+                  key={index}
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
+            {Coords[8].parkingspaces.map((value, index) => {
+              return (
+                <img
+                id="sideways"
+                  key={index}
+                  src={GreyBus}
+                  style={{ left: value.x, top: value.y }}
+                />
+              );
+            })}
           </div>
+        </div>
       </div>
     </div>
   );
