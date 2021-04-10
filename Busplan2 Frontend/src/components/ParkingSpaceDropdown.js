d606246323c5cac563126eb668bb238765142e30
Noah Koole <Noahkoole@gmail.com>
2021-04-10 17:34:47 +0200

    Fixed visual parkingspace update dropdown

--- Busplan2 Frontend/src/components/ParkingSpaceDropdown.js
+++ Busplan2 Frontend/src/components/ParkingSpaceDropdown.js
@@ -4,31 +4,20 @@ const Coords = require("../components/ParkingSpaceCoords.json");
 const ParkingSpaceDropdown = ({ spaces }) => {
 
     const SelectDecider = ({ space }) => {
-        if (space.type == 0) {
-            for (var i = 0; i < Coords[space.type].parkingspaces.length; i++) {
-                if (space.number == Coords[space.type].parkingspaces[i].parkingNumber) {
-                    return <option value={space.parkingSpaceID}>Normaal P{space.number}</option>
-                }
-            }
-        } else if (space.type == 1) {
-            for (var i = 0; i < Coords[space.type].parkingspaces.length; i++) {
-                if (space.number == Coords[space.type].parkingspaces[i].parkingNumber) {
-                    return <option value={space.parkingSpaceID}>Snellader P{space.number}</option>
-                }
-            }
-        } else if (space.type == 2) {
-            for (var i = 0; i < Coords[space.type].parkingspaces.length; i++) {
-                if (space.number == Coords[space.type].parkingspaces[i].parkingNumber) {
-                    return <option value={space.parkingSpaceID}>Langzaamlader P{space.number}</option>
-                }
-            }
-            for (var i = 0; i < Coords[3].parkingspaces.length; i++) {
-                if (space.number == Coords[3].parkingspaces[i].parkingNumber) {
-                    return <option value={space.parkingSpaceID}>Langzaamlader P{space.number}</option>
-                }
-            }
-        }
-        else return null;
+        if (space.type == 0)
+            return <option value={space.parkingSpaceID}>Normaal P{space.number}</option>
+        if (space.type == 1)
+            return <option value={space.parkingSpaceID}>Snellader P{space.number}</option>
+        if (space.type == 2)
+            return <option value={space.parkingSpaceID}>Langzaamlader P{space.number}</option>
+        if (space.type == 3)
+            return <option value={space.parkingSpaceID}>Onderhoud P{space.number}</option>
+        if (space.type == 4)
+            return <option value={space.parkingSpaceID}>Wasstraat P{space.number}</option>
+        if (space.type == 5)
+            return <option value={space.parkingSpaceID}>Buitengebruik P{space.number}</option>
+        if (space.type == 6)
+            return <option value={space.parkingSpaceID}>Reservemateriaal P{space.number}</option>
     }
 
     return (
